c3e03fdc4c7c27d013be66094ad0842c3aa02a8a
Erika Lena <elena@login.hpc>
2021-11-14 18:58:41 +0100

    up

--- section2/benchmarks.sh
+++ section2/benchmarks.sh
@@ -11,7 +11,7 @@ module load openmpi-4.1.1+gnu-9.3.0
 
 # map by core 
 mpirun -np 2 --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_ucx.csv
-mpirun -mca pml ob1 --mca btl self,tcp --report-bindings -np 2 --map-by core ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_tcp.csv  
+mpirun -np 2 -mca pml ob1 --mca btl self,tcp --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_tcp.csv  
 mpirun -np 2 -mca pml ob1 --mca btl self,vader --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_vader.csv
 
 # map by socket
