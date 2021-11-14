fa8c32e2f3676d15af3fe574a595768e580919b7
Erika Lena <lena.erika@gmail.com>
2021-11-14 16:32:51 +0100

    up

--- section2/benchmarks.sh
+++ section2/benchmarks.sh
@@ -10,14 +10,25 @@ cd $HOME/mpi_bench/mpi_benchmarks/src_c
 module load openmpi-4.1.1+gnu-9.3.0
 
 # map by core 
-mpirun -np 2 --report-bindings ./IMB-MP1 PingPong 2>/dev/null
+mpirun -np 2 --report-bindings ./IMB-MP1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_ucx.csv
+
+mpirun -np 2 -mca pml ob1 --mca btl self,tcp  --report-bindings ./IMB-MPI1 PingPong 2>/dev/null  grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_tcp.csv 
+
+mpirun -np 2 -mca pml ob1 --mca btl self,vader --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > core_vader.csv
 
 # map by socket
-mpirun -np 2 --map-by socket --report-bindings ./IMB-MPI1 PingPong 2>/dev/null
+#mpirun -np 2 --map-by socket --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > socket_ucx.csv 
+
+#mpirun -np 2 -mca pml ob1 --mca btl self,tcp  --map-by socket --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > socket_tcp.csv
+
+#mpirun -np 2 -mca pml ob1 --mca btl self,vader  --map-by socket --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > socket_vader.csv
 
 
 # map by node
-mpirun -np 2 --map-by node --report-bindings ./IMB-MPI1 PingPong 2>/dev/null
+#mpirun -np 2 --map-by node --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > node_ucx.csv
+
+#mpirun -np -mca pml ob1 --mca btl self,tcp  --map-by node --report-bindings ./IMB-MPI1 PingPong 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > node_tcp.csv
+
 
 
 exit
