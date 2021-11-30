295cb73b471a215fb0758a3592a69d2aec77f4cf
erika.lena@outlook.it <erika.lena@outlook.it>
2021-11-30 16:51:28 +0100

    intel

--- section2/intel_bench.sh
+++ section2/intel_bench.sh
@@ -6,9 +6,16 @@
 # move to mpi benchmark dir
 cd $PBS_O_WORKDIR
 
-# load openmpi module
+# load intel module
 module load intel/20.4
 
-mpiexec.hydra -n 2 -iface ib0 ./IMB-MPI1_intel PingPong
+# map by core 
+mpiexec -n 2 I_MPI_PIN_PROCESSOR_LIST=0,2 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_core_ucx.csv
+
+# map by socket
+mpiexec -n 2 I_MPI_PIN_PROCESSOR_LIST=0,1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_socket_ucx.csv
+
+# map by node
+mpiexec -n 2 -ppn 1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_node_ucx.csv
 
 exit
