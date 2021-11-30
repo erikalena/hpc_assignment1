3d9b180ea787bab85a040d0f14a12fc4a8afdd46
Erika Lena <elena@login.hpc>
2021-11-30 17:17:29 +0100

    intel

--- section2/intel_bench.sh
+++ section2/intel_bench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l nodes=2:ppn=24
+#PBS -l nodes=2:ppn=2
 #PBS -l walltime=0:30:00
 #PBS -q dssc
 
@@ -10,10 +10,10 @@ cd $PBS_O_WORKDIR
 module load intel/20.4
 
 # map by core 
-mpiexec -n 2 I_MPI_PIN_PROCESSOR_LIST=0,2 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_core_ucx.csv
+mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_core_ucx.csv
 
 # map by socket
-mpiexec -n 2 I_MPI_PIN_PROCESSOR_LIST=0,1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_socket_ucx.csv
+mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_socket_ucx.csv
 
 # map by node
 mpiexec -n 2 -ppn 1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_node_ucx.csv
