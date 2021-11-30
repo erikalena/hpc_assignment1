15d3aa14d38758632ab27dd1b8a58ac144b0daad
erika.lena@outlook.it <erika.lena@outlook.it>
2021-11-30 17:55:21 +0100

    tcp

--- section2/intel_bench.sh
+++ section2/intel_bench.sh
@@ -11,11 +11,16 @@ module load intel/20.4
 
 # map by core 
 mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_core_ucx.csv
+mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 I_MPI_OFI_PROVIDER tcp ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_core_tcp.csv
+mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 I_MPI_OFI_PROVIDER shm ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_core_shm.csv
 
 # map by socket
 mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_socket_ucx.csv
+mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 I_MPI_OFI_PROVIDER tcp  ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_socket_tcp.csv
+mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 I_MPI_OFI_PROVIDER shm  ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_socket_shm.csv
 
 # map by node
 mpiexec -n 2 -ppn 1 ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_node_ucx.csv
+mpiexec -n 2 -ppn 1 I_MPI_OFI_PROVIDER tcp ./IMB-MPI1_intel PingPong -msglog 30 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > intel_node_tcp.csv
 
 exit
