a745f2aeb7f7e18da212a6124e61b0a4988c9203
Erika Lena <elena@login.hpc>
2021-12-01 17:59:25 +0100

    bash

--- section3/jacobi.sh
+++ section3/jacobi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l nodes=2:ppn=24
+#PBS -l nodes=1:ppn=8
 #PBS -l walltime=0:30:00
 #PBS -q dssc
 
@@ -15,12 +15,14 @@ mpif77 -ffixed-line-length-none Jacobi_MPI_vectormode.F -o jacoby3D.x
 printf '%s\t%s\t%s\t%s\t%s\t%s\n' 'map' 'n_procs' 'maxtime' 'maxtime' 'jacobimin' 'jacobimax' >> results.csv
 
 # run the code
-i=2
+i=1
 #for i in {1..3}
 #do 
-	printf 'core\t%d\t%s\t%s\t%s\t%s\n' ${i} $(mpirun --mca btl ^openib -np 4*{i} -map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122)
+	((n=4*i))
+	printf 'core\t%d\t' $n >> results.csv
+	mpirun --mca btl ^openib -np ${n} --map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 >> results.csv
 	
-	#mpirun --mca btl ^openib -np 4*{i} -map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122
+	#mpirun --mca btl ^openib -np $n -map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122
 
 #done
 
