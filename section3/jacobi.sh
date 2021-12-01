98b1613363791ae86a4dc6f463df2f980cf9d852
Erika Lena <elena@login.hpc>
2021-12-01 19:25:33 +0100

    bash

--- section3/jacobi.sh
+++ section3/jacobi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l nodes=2:ppn=24
-#PBS -l walltime=0:10:00
+#PBS -l nodes=1:ppn=12
+#PBS -l walltime=0:30:00
 #PBS -q dssc
 
 # move to mpi benchmark dir
@@ -12,13 +12,13 @@ module load openmpi-4.1.1+gnu-9.3.0
 # compile
 mpif77 -ffixed-line-length-none Jacobi_MPI_vectormode.F -o jacoby3D.x
 
-printf '%s\t%s\t%s\t%s\t%s\t%s\n' 'map' 'n_procs' 'maxtime' 'maxtime' 'jacobimin' 'jacobimax' >> results.csv
+printf '%s,%s,%s,%s,%s,%s\n' 'map' 'n_procs' 'maxtime' 'maxtime' 'jacobimin' 'jacobimax' >> results.csv
 
 # run the code
 for i in {1..3}
 do 
 	((n=4*i))
-	printf 'core\t%d\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 |  sed 's/ \{1,\}/,/g'` >> results.csv
+	printf 'core,%d%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 |  sed 's/ \{1,\}/,/g'` >> results.csv
 done
 
 
@@ -36,6 +36,6 @@ done
 
 
 rm jacobi.sh.*
-rm task*
+rm task.*
 
 exit
