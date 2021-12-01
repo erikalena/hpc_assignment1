65c507e13f3148d4bee8dc39521d9e9a5d75627b
Erika Lena <lena.erika@gmail.com>
2021-12-01 18:16:23 +0100

    bash

--- section3/jacobi.sh
+++ section3/jacobi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=8
-#PBS -l walltime=0:30:00
+#PBS -l nodes=2:ppn=24
+#PBS -l walltime=1:00:00
 #PBS -q dssc
 
 # move to mpi benchmark dir
@@ -15,14 +15,24 @@ mpif77 -ffixed-line-length-none Jacobi_MPI_vectormode.F -o jacoby3D.x
 printf '%s\t%s\t%s\t%s\t%s\t%s\n' 'map' 'n_procs' 'maxtime' 'maxtime' 'jacobimin' 'jacobimax' >> results.csv
 
 # run the code
-i=1
-#for i in {1..3}
-#do 
+for i in {1..3}
+do 
 	((n=4*i))
-	printf 'core\t%d\t%s\t%s\t%s\t%s\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122` >> results.csv
-	
-	#mpirun --mca btl ^openib -np $n -map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122
+	printf 'core\t%d\t%s\t%s\t%s\t%s\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by core ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/[ \t]+*/,/g'` >> results.csv
+done
 
-#done
+for i in {1..3}
+do 
+	printf 'socket\t%d\t%s\t%s\t%s\t%s\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by socket ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/[ \t]+*/,/g'` >> results.csv
+done
+
+for i in {1..3}
+do 
+	((n=12*i))
+	printf 'core\t%d\t%s\t%s\t%s\t%s\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by node ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/[ \t]+*/,/g'` >> results.csv
+done
+
+rm jacobi.sh.*
+rm task*
 
 exit
