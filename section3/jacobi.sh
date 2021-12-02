074d0bcb3b4fcb5d078ce153b02755f9b8e16bf9
Erika Lena <elena@login.hpc>
2021-12-02 09:44:10 +0100

    jacobi_res

--- section3/jacobi.sh
+++ section3/jacobi.sh
@@ -14,7 +14,10 @@ mpif77 -ffixed-line-length-none Jacobi_MPI_vectormode.F -o jacoby3D.x
 
 printf '%s,%s,%s,%s,%s,%s\n' 'map' 'n_procs' 'maxtime' 'maxtime' 'jacobimin' 'jacobimax' >> results.csv
 
-# run the code
+# run the code on one single core to estimate serial time
+printf 'core,1%s\n' `mpirun --mca btl ^openib -np 1 ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 |  sed 's/ \{1,\}/,/g'` >> results.csv
+
+# run the code on 4/8/12 processes within the same node pinning MPI processes within same socket and across two sockets
 for i in {1..3}
 do 
 	((n=4*i))
@@ -24,13 +27,15 @@ done
 
 for i in {1..3}
 do 
-	printf 'socket\t%d\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by socket ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/ \{1,\}/,/g'` >> results.csv
+	((n=4*i))
+	printf 'socket,%d%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by socket ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/ \{1,\}/,/g'` >> results.csv
 done
 
-for i in {1..3}
+# run the code on 12/24/36/48 processors using two nodes
+for i in {1..4}
 do 
 	((n=12*i))
-	printf 'core\t%d\t%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by node ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/ \{1,\}/,/g'` >> results.csv
+	printf 'core,%d%s\n' $n `mpirun --mca btl ^openib -np ${n} --map-by node ./jacoby3D.x < input.1200 | tail -n 1 | cut -c 46- | cut --complement -c 84-122 | sed 's/ \{1,\}/,/g'` >> results.csv
 done
 
 
