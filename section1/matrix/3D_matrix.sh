b3cebbb4c08553dc7f7a206ebcbca86f43845431
Erika Lena <elena@login.hpc>
2021-12-11 16:20:11 +0100

    matrix results

--- section1/matrix/3D_matrix.sh
+++ section1/matrix/3D_matrix.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=01:00:00
+#PBS -q dssc
+
+cd $PBS_O_WORKDIR
+
 #load openmpi module
 module load openmpi-4.1.1+gnu-9.3.0
 
@@ -11,9 +17,9 @@ n=24
 printf '%s,%s,%s,\n' 'Matrix' 'Topology' 'Time taken' >> 3D_matrix.csv
 
 
-mpirun -np $n -oversubscribe ./sum3Dmatrix.x 2400 100 100
-mpirun -np $n -oversubscribe ./sum3Dmatrix.x 1200 200 100
-mpirun -np $n -oversubscribe ./sum3Dmatrix.x 800 300 100
+mpirun -np $n --mca btl ^openib ./sum3Dmatrix.x 2400 100 100
+mpirun -np $n --mca btl ^openib ./sum3Dmatrix.x 1200 200 100
+mpirun -np $n --mca btl ^openib ./sum3Dmatrix.x 800 300 100
 
 
 
