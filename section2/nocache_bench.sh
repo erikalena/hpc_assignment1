c391ba12d8d83787b32eb5aa81d2d731d1e1a1f5
erika.lena@outlook.it <erika.lena@outlook.it>
2021-12-24 19:44:04 +0100

    nocache tcp

--- section2/nocache_bench.sh
+++ section2/nocache_bench.sh
@@ -10,7 +10,15 @@ cd $PBS_O_WORKDIR
 module load openmpi-4.1.1+gnu-9.3.0
 
 # map by core 
-mpirun -np 2 --report-bindings ./IMB-MPI1 PingPong -off_cache -1  -msglog 28 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > csv/nocache_core_ucx.csv
+printf '%s\n' '# mpirun -np 2 --report-bindings ./IMB-MPI1 PingPong -off_cache -1  -msglog 28' > csv/nocache_core_ucx.csv
+mpirun -np 2 --report-bindings ./IMB-MPI1 PingPong -off_cache -1  -msglog 28 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >> csv/nocache_core_ucx.csv
+# tcp 
+printf '%s\n' '# mpirun -np 2 -mca pml ob1 --mca btl self,tcp  --map-by core --report-bindings ./IMB-MPI1 PingPong -msglog 28' > csv/nocache_core_tcp.csv
+mpirun -np 2 -mca pml ob1 --mca btl self,tcp  --map-by core --report-bindings ./IMB-MPI1 PingPong -msglog 28 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >> csv/nocache_core_tcp.csv
 
 # map by socket
-mpirun -np 2 --map-by socket --report-bindings ./IMB-MPI1 PingPong -off_cache -1 -msglog 28 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' > csv/nocache_socket_ucx.csv 
+printf '%s\n' '# mpirun -np 2 --map-by socket --report-bindings ./IMB-MPI1 PingPong -off_cache -1 -msglog 28' > csv/nocache_socket_ucx.csv
+mpirun -np 2 --map-by socket --report-bindings ./IMB-MPI1 PingPong -off_cache -1 -msglog 28 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >> csv/nocache_socket_ucx.csv 
+#tcp
+printf '%s\n' '# mpirun -np 2 -mca pml ob1 --mca btl self,tcp  --map-by socket --report-bindings ./IMB-MPI1 PingPong -msglog 28' > csv/nocache_socket_tcp.csv
+mpirun -np 2 -mca pml ob1 --mca btl self,tcp  --map-by socket --report-bindings ./IMB-MPI1 PingPong -msglog 28 2>/dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >> csv/nocache_socket_tcp.csv 
